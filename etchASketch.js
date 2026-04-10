b526716e323a5bbcc9670b12950bbc23a9aed33a
All-Over-J <oliverjennings5@gmail.com>
2026-04-10 12:11:57 -0500

    Create etch a sketch
    create grid squares and change the background
    colors of the squares on hover.

--- etchASketch.js
+++ etchASketch.js
@@ -1,8 +1,19 @@
 
 const gridContainer = document.querySelector(".gridContainer");
 
+//create 16 divs with the class "square" inside of
+//.gridcontainer.
 for (i=0; i<16; i++ ){
-const square = document.createElement("div");
-square.textContent = "x";
-gridContainer.appendChild(square);
+    const square = document.createElement("div");
+    // square.textContent = "x";
+    square.classList.add("square");
+    gridContainer.appendChild(square);
 }
+//add event listeners to all squares that add a 
+// purple background color on mouseover. 
+const squares = document.querySelectorAll(".square");
+squares.forEach(square => {
+    square.addEventListener("mouseover", ()=>{
+        square.style.backgroundColor = "purple";
+    });
+});
